35329e4b614c9972bf234a372795a6dba9143476
jiapril11 <tadadadacoding@gmail.com>
2023-05-31 14:30:16 +0900

    메인 배너 수정, 영화 리스트 스와이퍼 적용

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// TODO: 주석 달기
 const options = {
   method: "GET",
   headers: {
@@ -17,7 +18,6 @@ const topRatedMoviesFectch = fetch(
     const movieWrapper = document.querySelector(".top-rated-list");
 
     topRatedMovies.forEach((movie) => {
-      // console.log(movie);
       const id = movie.id;
       const title = movie.title;
       const overview = movie.overview;
@@ -25,11 +25,14 @@ const topRatedMoviesFectch = fetch(
       const voteAverage = movie.vote_average;
 
       const movieLi = document.createElement("li");
+      movieLi.classList.add("swiper-slide");
       movieLi.dataset.id = id;
       movieLi.innerHTML = `
-                        <img src="${posterURI}" alt="${title} 포스터 이미지">
-                        <h3>${title}</h3>
-                        <span>${voteAverage}</span>
+                        <div class="poster-img" style="background: center / cover url('${posterURI}') no-repeat;" title="${title} 포스터 이미지"></div>
+                        <div class="main-info">
+                          <h3>${title}</h3>
+                          <span>${voteAverage}</span>
+                        </div>
                         <p>${overview}</p>
                       `;
       movieWrapper.append(movieLi);
@@ -41,11 +44,39 @@ const topRatedMoviesFectch = fetch(
 
     searchForm.addEventListener("submit", (e) => {
       e.preventDefault();
-      const searchItem = document.querySelector(".search-input").value;
-      const matchItem = topRatedMovies.filter(
-        (topRatedMovie) => topRatedMovie.title === searchItem
-      );
-      console.log(matchItem[0]);
+      const searchInput = document.querySelector(".search-input");
+      let searchText = searchInput.value;
+
+      if (searchText.length <= 0) {
+        alert("검색어를 입력해주세요");
+        searchInput.focus();
+      } else {
+        const matchItem = topRatedMovies.filter(
+          (topRatedMovie) => topRatedMovie.title === searchText
+        );
+        searchInput.value = "";
+
+        if (!matchItem[0]) {
+          alert("일치하는 영화가 없습니다.");
+          searchInput.focus();
+          return;
+        }
+        return console.log(matchItem[0]);
+      }
     });
   })
   .catch((err) => console.error(err));
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.querySelector(".topRatedSwiper")) {
+    var topRatedSwiper = new Swiper(".topRatedSwiper", {
+      slidesPerView: 6,
+      spaceBetween: 30,
+      slidesPerGroup: 6,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+});
